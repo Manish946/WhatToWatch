1baffa6323ac798cb01df9ea6387c8ec3df800b7
Manish946 <manishestha@gmail.com>
2024-04-05 23:10:09 +0200

    Register and login setup.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,5 @@ dependencies {
     implementation(libs.lifecycle.extensions)
     implementation(libs.converter.gson.retrofit)
     implementation(libs.picasso)
+    implementation(libs.bcrypt)
 }
