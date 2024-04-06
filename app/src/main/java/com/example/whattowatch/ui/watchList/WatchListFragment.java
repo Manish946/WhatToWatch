b4c0316482c0d90ceab7a5d2f0b998a1bea44dc2
Manish946 <manishestha@gmail.com>
2024-04-06 19:49:22 +0200

    Refactored dashboard to Watchlist

--- app/src/main/java/com/example/whattowatch/ui/watchList/WatchListFragment.java
+++ app/src/main/java/com/example/whattowatch/ui/watchList/WatchListFragment.java
@@ -1,4 +1,4 @@
-package com.example.whattowatch.ui.dashboard;
+package com.example.whattowatch.ui.watchList;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.whattowatch.databinding.FragmentDashboardBinding;
+import com.example.whattowatch.databinding.FragmentWatchlistBinding;
 
-public class DashboardFragment extends Fragment {
+public class WatchListFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentWatchlistBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        WatchListViewModel watchListViewModel =
+                new ViewModelProvider(this).get(WatchListViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentWatchlistBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        watchListViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/com/example/whattowatch/ui/watchList/WatchListViewModel.java
+++ app/src/main/java/com/example/whattowatch/ui/watchList/WatchListViewModel.java
@@ -1,14 +1,14 @@
-package com.example.whattowatch.ui.dashboard;
+package com.example.whattowatch.ui.watchList;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class WatchListViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public WatchListViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is dashboard fragment");
 
